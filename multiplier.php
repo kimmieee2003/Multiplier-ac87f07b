3448d157de358f4e5b6d00bd99f0b34e02e3c7ad
Kimberly Hogeveen <2103405@talnet.nl>
2020-03-18 08:59:26 +0100

    verbeter versie

--- multiplier.php
+++ multiplier.php
@@ -1,5 +1,5 @@
 <?php
-echo "we gaan 2 getallen met elkaar vermenigvuldigen".  " ";
+echo 'we gaan 2 getallen met elkaar vermenigvuldigen'.  ' ' ;
 $getal1 = readline("Geef je eerste getal op");
-$getal2 = readline("Geef je tweede getal op");
+$getal1 = readline("Geef je eerste getal op");
 echo "de uitkomst is " . $getal1 * $getal2;
